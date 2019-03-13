437fa43284e8924b8f49eeaba537869564ea8bb9
Lars-August Udnesseter Johnson <Lars-August@Lars-Augusts-MacBook-Pro.local>
2019-03-13 18:43:15 +0100

    made progress on about me

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import M from 'materialize-css';
+
 import background1 from './assets/background1.jpg';
 import fiskinfo from './assets/FiskInfo.gif';
 import background2 from './assets/background2.jpg';
 import background3 from './assets/background3.jpg';
 import laJohnLogo from './assets/LaJohnLogo.png';
-
-
+import pbilde from './assets/P.Bilde.jpg';
 import './App.css';
 
 class App extends Component {
@@ -14,7 +14,6 @@ class App extends Component {
 
   render() {
     return (
-
       <div className="App">
       <nav className="">
         <div className="nav-wrapper">
@@ -38,12 +37,13 @@ class App extends Component {
           Welcome to my portfolio
           </h3>
           <p id="quote">
-          “Walking on water and developing software from a specification are easy if both are frozen.”
+            “Walking on water and developing software from a specification
           </p>
-          <p id="quoter">
-          ―Edward V. Berard
+          <p id="quote">
+            are easy if both are frozen.” ―Edward V. Berard
           </p>
-              <br />
+          <br/>
+          <br/>
           <a target="_blank"
             href="https://www.linkedin.com/in/lars-august-johnson-a9245b180/"
             className="waves-effect wawes blue btn target=_blank">
@@ -58,38 +58,61 @@ class App extends Component {
 
         {/* lager en rad av 3 er 4/12 store */}
         <div id="about" className="row">
-          <div className="row">
             <div class="col s12">
-              <h3 id="title">#About Lars-August</h3>
+              <h3 id="titles">#About Lars-August</h3>
             </div>
-          </div>
-          <div class="col s4">
-            <img src={fiskinfo} className=""/>
-            <br />
-            <i class="material-icons">visibility_off</i>
-            <p>source private</p>
-          </div>
-          <div class="col s4">
-            heihopp
-          </div>
-          <div class="col s4">
-            heihopp
-          </div>
-        </div>
+            <div id="pbilde" className="col s2 offset-s1">
+                <img src={pbilde} className="circle responsive-img"/>
+            </div>
+            <div  class="col s6">
+              <p id="abouttxt">
+                Nam molestie quis ligula at varius. Nam id sapien diam.Sed ipsum enim, lobortis vel volutpat ornare, laoreet eget sapien. Quisque magna ante, pretium eu est in, dictum bibendum arcu.
+                Nam id sapien diam. Sed ipsum enim, lobortis vel volutpat ornare, laoreet eget sapien. Quisque magna ante, pretium eu est in, dictum bibendum arcu.
+              </p>
+              <p id="abouttxt">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut dui at nunc lacinia condimentum eget ut risus. Mauris egestas dignissim rutrum. Duis sit amet suscipit turpis. Fusce dui ante, semper nec purus ac, varius rhoncus enim. In in pellentesque arcu, sit amet luctus dolor. Mauris at eleifend nisl. Integer eget condimentum augue. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                Integer tempor orci est, eu mollis sem egestas nec. Proin eu scelerisque arcu. Donec vitae lobortis massa, id efficitur neque. Sed bibendum est enim, vel luctus sapien faucibus vel. Sed pharetra, dolor a viverra consequat, arcu mauris molestie mauris, sed placerat nibh odio ac nisi. Suspendisse nulla odio, efficitur eleifend facilisis consequat, accumsan ac risus. Donec tempor et felis vitae efficitur. Donec et mollis sapien. Aenean consequat ornare enim sed sodales. Nulla   tempor velit nibh, a tempus dui maximus sed. Nulla eget vestibulum mi, eu laoreet ligula.</p>
+              <br />
+
+         </div>
+
+
+
+      </div>
+
+
+
+
         {/* lager en rad av 3 er 4/12 store */}
         <div id="skills" class="row">
-          <div class="col s4">
-            <img src={fiskinfo} className=""/>
-            <br />
-            <i class="material-icons">visibility_off</i>
-          </div>
-          <div class="col s4">
-            heihopp
-          </div>
-          <div class="col s4">
-            heihopp
+        <div  class="col s12">
+          <h3 id="titles2">#Skills Lars-August</h3>
+        </div>
+        <div id="cards" class="col s12 m2">
+          <div class="card-panel white">
+            <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+            </span>
           </div>
         </div>
+        <div class="col s12 m2">
+          <div class="card-panel white">
+            <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I rI am a very simple card. I am good at containing small bits of information.
+            I am convenient because I equire little markup to use effectively. I am similar to what is called a panel in other frameworks.
+            </span>
+          </div>
+        </div>
+        <div class="col s12 m2">
+          <div class="card-panel white">
+            <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+            </span>
+          </div>
+        </div>
+        </div>
+
+
         {/* lager en rad av 3 er 4/12 store */}
         <div id="projects" className="row">
           <div class="col s4">
@@ -107,10 +130,6 @@ class App extends Component {
         <div className="col12">
           <i id="up" className="medium material-icons"><a href="#logo">expand_less</a></i>
         </div>
-
-
-
-
 
 
 {/*app closer*/}
