16c6ce5bf8f627d9a435f3dd399e88641b8dcf35
Lars-August Udnesseter Johnson <Lars-August@Lars-Augusts-MacBook-Pro.local>
2019-09-04 18:00:50 +0200

    added a project and madde some minor changes.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,82 +1,86 @@
-import React, { Component } from 'react';
-import M from 'materialize-css';
-import Projects from './Projects/Projects'
-import About from './about/About'
-import Skills from './skills/Skills'
-import Footer from './footer/Footer'
-import Navigation from './navigation/navigation'
+import React, { Component } from "react";
+import M from "materialize-css";
+import Projects from "./Projects/Projects";
+import About from "./about/About";
+import Skills from "./skills/Skills";
+import Footer from "./footer/Footer";
+import Navigation from "./navigation/navigation";
 
-import Typed from 'react-typed'
+import Typed from "react-typed";
 
-import laJohnLogo from './images/logo3.png';
-import background from './images/background.png';
-import background2 from './images/background2.jpg';
+import laJohnLogo from "./images/logo3.png";
+import background from "./images/background.png";
+import background2 from "./images/background2.jpg";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    document.getElementById("appHeader").scrollIntoView();
+  }
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef(); // Create a ref object
+  }
 
-  componentDidMount(){
-    document.getElementById('appHeader').scrollIntoView();
-    }
-    constructor(props) {
-       super(props)
-       this.myRef = React.createRef()   // Create a ref object
-   }
-
-   scrollToMyRef = () => window.scrollTo(0, this.myRef.current.offsetTop)
+  scrollToMyRef = () => window.scrollTo(0, this.myRef.current.offsetTop);
 
   render() {
     return (
-
       <div className="App" id="App">
         <div className="background">
-          <img id="logo"
+          <img
+            id="logo"
             onClick={this.scrollToMyRef}
             className="left brand-logo"
             src={laJohnLogo}
           />
-            <div id="appHeader"
-             className="App-header"
-             ref={this.myRef}
-            >
-              <p id="title">Welcome to my<span id="space"></span>
+          <div id="appHeader" className="App-header" ref={this.myRef}>
+            <p id="title">
+              Welcome to my<span id="space"></span>
               <Typed
-                  strings={[
-                  ' ^1000 portfolio ^1000',
-                  ' ReactJS Application ^1000',
-                  ' web page ^1000',
-                  ' portfolio '
-                  ]}
-                  value='stop'
-                  typeSpeed={70}
-                  backSpeed={90}
+                strings={[
+                  " ^1000 portfolio ^1000",
+                  " ReactJS Application ^1000",
+                  " web page ^1000",
+                  " portfolio "
+                ]}
+                value="stop"
+                typeSpeed={70}
+                backSpeed={90}
               />
-              </p>
-              <p id="small_title">Welcome to my portfolio!</p>
-              <div id="row_nogap" className="row">
-                <div className="col l12 s9 offset-s1 ">
-                  <p id="quote">
-                    “Walking on water and developing software from a specification
-                  </p>
-                  <p id="quote">
-                    are easy if both are frozen.” ―Edward V. Berard
-                  </p>
-                </div>
-              </div>
-              <a id="get_in_touch" className="button" href="https://www.linkedin.com/in/lars-august-johnson-a9245b180/" target="_blank">Get in touch</a>
-              <div className="expand">
-                <i className="medium material-icons">
-                  <a href="#About">expand_more</a>
-                </i>
+            </p>
+            <p id="small_title">Welcome to my portfolio!</p>
+            <div id="row_nogap" className="row">
+              <div className="col l12 s9 offset-s1 ">
+                <p id="quote">
+                  “Walking on water and developing software from a specification
+                </p>
+                <p id="quote">
+                  are easy if both are frozen.” ―Edward V. Berard
+                </p>
               </div>
             </div>
+            <a
+              id="get_in_touch"
+              className="button"
+              href=" https://www.linkedin.com/in/lars-august-udnesseter-johnson-a9245b180/"
+              target="_blank"
+            >
+              Get in touch
+            </a>
+            <div className="expand">
+              <i className="medium material-icons">
+                <a href="#About">expand_more</a>
+              </i>
+            </div>
           </div>
-          {/*<Navigation />*/}
-          <About />
-          <Skills />
-          <Projects />
-          <Footer />
+        </div>
+        {/*<Navigation />*/}
+        <About />
+        <Skills />
+        <Projects />
+        <Footer />
         {/*app closer*/}
       </div>
     );
--- my-app/src/Projects/Projects.js
+++ my-app/src/Projects/Projects.js
@@ -1,40 +1,72 @@
-import React, { Component } from 'react';
-import M from 'materialize-css';
-import './Projects.css';
+import React, { Component } from "react";
+import M from "materialize-css";
+import "./Projects.css";
 
-import fiskinfo from '../images/FiskInfo.gif';
-import heatmap from '../images/heatmap.png';
-import todo from '../images/todo.png';
-import github from '../images/github.svg';
-import posicom from '../images/posicom.png';
-import portfolio from '../images/portfolio2.png';
-import weather from '../images/weather.png';
-
+import fiskinfo from "../images/FiskInfo.gif";
+import heatmap from "../images/heatmap.png";
+import todo from "../images/todo.png";
+import github from "../images/github.svg";
+import posicom from "../images/posicom.png";
+import portfolio from "../images/portfolio2.png";
+import weather from "../images/weather.png";
+import Brannkonsept from "../images/brannkonsept.png";
 
 export default class Projetcs extends Component {
   render() {
     return (
-
       <div className="Projects">
         <div id="projects" className="">
           <br />
           <h3 id="titles">Previous projects</h3>
-          <div className="col s12 ">
+          <div className="col s12 "></div>
+          <div id="aboutProject" class="row">
+            <div class="col s12 m12 l6 offset-l1">
+              <p id="p_title"> Digitalt brannkonsept - Rambøll</p>
+              <img id="ProjectImg" src={Brannkonsept} className="project_img" />
+              <br />
+              <i id="visability" class="material-icons">
+                visibility_off
+              </i>
+              Source code private
+              <br />
+            </div>
+            <div className="col s8 m8 l4 offset-s2 offset-m2">
+              <div>
+                <p id="abouttxt">
+                  During the summer of 2019 I was hired by Rambøll as a summer
+                  intern. During my time at Rambøll I created a React web app
+                  that collects data from the user through a large form. This
+                  data is then used to generate a customized word document by
+                  using the Python package python-docx-template. This product
+                  was created to increase the workflow of Fire and Safety
+                  engineers at Rambøll.
+                </p>
+              </div>
+            </div>
           </div>
+          <hr className="pageSep" />
           <div id="aboutProject" className="row">
             <div class="col s12 m12 l6 offset-l1">
               <p id="p_title">FiskInfo - SINTEF Digital</p>
-              <img id="ProjectImg"
-                src={heatmap}
-                className="project_img"
-                />
-                <br />
-                <i id="visability" class="material-icons">visibility_off</i> Source code protected by NDA
+              <img id="ProjectImg" src={heatmap} className="project_img" />
+              <br />
+              <i id="visability" class="material-icons">
+                visibility_off
+              </i>
+              Source code protected by NDA
             </div>
             <div className="col s8 m8 l4 offset-s2 offset-m2">
               <div className="textContainer">
                 <p id="abouttxt">
-                  Through the course IT2901 a group of seven students were hired by SINTEF Digital to develop the application FiskInfo as a part of the bachelor thesis. The application’s purpose is to aggregate, gather and display useful information for the fishing industry using coordinated views to connect the data of graphs and maps. The project delivered a functional proof of concept build with development quality. Keywords for this projects are; dc.js, d3.js, crossfilter.js, OpenLayers4, python.
+                  Through the course IT2901 a group of seven students were hired
+                  by SINTEF Digital to develop the application FiskInfo as a
+                  part of the bachelor thesis. The application’s purpose is to
+                  aggregate, gather and display useful information for the
+                  fishing industry using coordinated views to connect the data
+                  of graphs and maps. The project delivered a functional proof
+                  of concept build with development quality. Keywords for this
+                  projects are; dc.js, d3.js, crossfilter.js, OpenLayers4,
+                  python.
                 </p>
               </div>
             </div>
@@ -43,28 +75,30 @@ export default class Projetcs extends Component {
           <div id="aboutProject" class="row">
             <div class="col s12 m12 l6 offset-l1">
               <p id="p_title"> My Portfilio (This page)</p>
-              <img id="ProjectImg"
-                src={portfolio}
-                className="project_img"
-                />
-                <br />
-                <i id="visability" class="material-icons">visibility</i> Source code available
-                <br />
-                <a target="_blank"
+              <img id="ProjectImg" src={portfolio} className="project_img" />
+              <br />
+              <i id="visability" class="material-icons">
+                visibility
+              </i>
+              Source code available
+              <br />
+              <a
+                target="_blank"
                 href="https://github.com/lajohnso/lajohn.tech"
-                className= "floating pulse"
-                >
-                <img id="tiny"
-                src={github}
-                className=""
-                >
-              </img>
+                className="floating pulse"
+              >
+                <img id="tiny" src={github} className=""></img>
               </a>
             </div>
             <div className="col s8 m8 l4 offset-s2 offset-m2">
               <div>
                 <p id="abouttxt">
-                  This project was created to present some of my work as a web developer. It is a responsive website that uses grid styling from Materilize.css to ensure a smooth fit to any screen size. This site was created with ReactJS and it is hosted through Netlify. Visit my GitHub to have a closer look at my way of working, and the source code of this project.
+                  This project was created to present some of my work as a web
+                  developer. It is a responsive website that uses grid styling
+                  from Materilize.css to ensure a smooth fit to any screen size.
+                  This site was created with ReactJS and it is hosted through
+                  Netlify. Visit my GitHub to have a closer look at my way of
+                  working, and the source code of this project.
                 </p>
               </div>
             </div>
@@ -73,64 +107,75 @@ export default class Projetcs extends Component {
           <div id="aboutProject" class="row">
             <div class="col s12 m6 l6 offset-l1 offset-m3">
               <p id="p_title">Posicom.no</p>
-              <img id="ProjectImg"
-                src={posicom}
-                className="project_img"
-              />
+              <img id="ProjectImg" src={posicom} className="project_img" />
               <br />
-              <i id="visability" class="material-icons">visibility_off</i> Source code private
+              <i id="visability" class="material-icons">
+                visibility_off
+              </i>
+              Source code private
             </div>
             <div class="col s8 m8 l4 offset-s2 offset-m2">
               <div>
                 <p id="abouttxt">
-                  Created a website for Posicom AS. In 2017 I had just finished the first year of my computer science degree, which led me to my first web development project. Posicom AS needed to update their website, which was outdated. They asked me to develop a new page in Wordpress, to limit maintaince costs. Throughout this project I got experience with customer-client communication, wordpress, php and more.
+                  Created a website for Posicom AS. In 2017 I had just finished
+                  the first year of my computer science degree, which led me to
+                  my first web development project. Posicom AS needed to update
+                  their website, which was outdated. They asked me to develop a
+                  new page in Wordpress, to limit maintaince costs. Throughout
+                  this project I got experience with customer-client
+                  communication, wordpress, php and more.
                 </p>
               </div>
             </div>
           </div>
           <hr className="pageSep" />
-          <div id="aboutProject"  class="row">
+          <div id="aboutProject" class="row">
             <div class="col s12 m6 l6 offset-l1 offset-m3">
               <p id="p_title">Weather application - Private Project</p>
-              <img id="ProjectImg"
-                src={weather}
-                className="project_img"
-              />
+              <img id="ProjectImg" src={weather} className="project_img" />
               <br />
-              <i id="visability" class="material-icons">visibility</i> Source code available
+              <i id="visability" class="material-icons">
+                visibility
+              </i>
+              Source code available
               <br />
-              <a target="_blank"
+              <a
+                target="_blank"
                 href="https://github.com/lajohnso/lajohn.tech"
-                className= "floating pulse">
-                <img id="tiny"
-                  src={github}
-                  className=""
-                  >
-                </img>
+                className="floating pulse"
+              >
+                <img id="tiny" src={github} className=""></img>
               </a>
             </div>
             <div class="col s8 m8 l4 offset-s2 offset-m2">
               <div className="adjust">
                 <p id="abouttxt">
-                  The project "Weather application" was all about learning how to communicate with an external API. In this case, I collected the data from
-                   <a id="link"
+                  The project "Weather application" was all about learning how
+                  to communicate with an external API. In this case, I collected
+                  the data from
+                  <a
+                    id="link"
                     href="https://wwww.openweathermap.org"
-                    target="_blank">
+                    target="_blank"
+                  >
                     openweathermap.org
                   </a>
-                  by using a personal API key. In my project I converted the data to json objects. Inspiration from github user hamza-mirza. Keywords for this project are ReactJS, API-calls, json.
+                  by using a personal API key. In my project I converted the
+                  data to json objects. Inspiration from github user
+                  hamza-mirza. Keywords for this project are ReactJS, API-calls,
+                  json.
                 </p>
               </div>
             </div>
           </div>
         </div>
 
-        <div id= "upwards" className="col1 offset s11">
-          <i id="up" className="medium material-icons"><a href="#App">expand_less</a></i>
+        <div id="upwards" className="col1 offset s11">
+          <i id="up" className="medium material-icons">
+            <a href="#App">expand_less</a>
+          </i>
         </div>
       </div>
-
-
-    )
+    );
   }
 }
